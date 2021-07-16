269caa79b510ab840f4ed34a13a1dfbe85bd2642
Javier Medina <javier.medina@upr.edu>
2021-07-16 00:04:05 -0400

    Adapting Starter Assets example 3D Controller to Skeleton Controller

---
+++ Assets/3D Movement With Animations/DungeonCharacters/Skeletons_demo/models/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+
+    private Vector3 moveDirection;
+
+    private CharacterController controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() 
+    {
+        float moveZ = 
+    }
+}

ddccd94301166f13a2824097053844a6ed483f4a
quichez <57053917+quichez@users.noreply.github.com>
2021-09-03 23:05:54 -0400

    Added TeammateTracker
    TeammateTracker WIP to track your teammates health (and more?)

---
+++ Assets/Levels/Arena1/Scripts/GUI/TeammateTracker.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quichez;
+using JAM;
+
+public class TeammateTracker : MonoBehaviour
+{
+    private PlayerManager _health;
+
+    
+}
